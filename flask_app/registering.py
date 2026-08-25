82212207d9e3b6d7d1f64adcf8cc5d0017bab87d
nico <ancion.nico@gmail.com>
2026-08-25 16:34:48 +0200

    removing some dev artefacts + adding CSRF protection, rate limit and cookies hardening

--- flask_app/registering.py
+++ flask_app/registering.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
 from shared.database_file.oltp_models import *
+from email_validator import validate_email, EmailNotValidError
 
 registering_bp = Blueprint("registering_bp", __name__)
 
@@ -9,20 +10,26 @@ def register():
 
         email = request.form.get("email")
         password = request.form.get("password")
-        pilotnumber = request.form.get("pilotnumber", "").strip()
         name = request.form.get("name")
         lastname = request.form.get("lastname")
 
+        try:
+            validate_email(email, check_deliverability=False).normalized
+        except EmailNotValidError:
+            flash("This email is already registered")
+            return redirect(url_for(".register"))
+
+        try:
+            pilotnumber = int(request.form.get("pilotnumber", "").strip())
+        except ValueError:
+            flash("Pilot number must be an integer", "error")
+            return redirect(url_for(".register"))
 
         if not all([email, password, pilotnumber, name, lastname]):
             flash("All fields are required")
             return redirect(url_for(".register"))
 
-        if Pilot.query.filter_by(pilot_email=email).first():
-            flash("This email is already registered")
-            return redirect(url_for(".register"))
-
-        elif Pilot.query.filter_by(pilot_number=pilotnumber).first():
+        if Pilot.query.filter_by(pilot_number=pilotnumber).first():
             flash("This pilot number is already registered")
             return redirect(url_for(".register"))
 
