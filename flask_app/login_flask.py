42918645598ed7f7dbe515b11aba11d9bf31e34f
nico <ancion.nico@gmail.com>
2026-08-25 16:34:28 +0200

    removing some dev artefacts + adding CSRF protection, rate limit and cookies hardening

--- flask_app/login_flask.py
+++ flask_app/login_flask.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
-from flask_login import login_user, logout_user, login_required, current_user
-from shared.database_file.set_up import Setup
-from flask_app.statistics import analytics
+from flask_login import login_user, logout_user, login_required
 from shared.database_file.oltp_models import *
+from flask_app.main import limiter
 
 login_bp = Blueprint("login_bp", __name__)
 
 @login_bp.route("/login", methods=["GET", "POST"])
+@limiter.limit("5 per minute", methods=["POST"])
 def login():
     if request.method == "POST":
         userlogin = request.form["email"]
@@ -15,19 +15,10 @@ def login():
         user = Pilot.query.filter_by(pilot_email=userlogin).first()
         if user and user.check_password(password):
             login_user(user)
-            return redirect(url_for("analytics.index", pilot_number=user.pilot_number))
-        # À mettre temporairement dans la route login après avoir récupéré 'user'
-        if user and not user.check_password(password):
-            # On ré-encode en direct pour tester si c'était un problème de vieux hash
-            print(f"Tentative de réparation pour {user.pilot_email}")
-            user.set_password(str(user.pilot_number).strip())
-            db.session.commit()
-            # On ré-essaie tout de suite
-            if user.check_password(password):
-                print("Réparation réussie !")
-        else:
-            flash("Invalid username or password")
-            return redirect(url_for("login_bp.login"))
+            return redirect(url_for("analytics.index"))
+
+        flash("Invalid username or password")
+        return redirect(url_for("login_bp.login"))
 
     return render_template("login.html")
 
